88580620f60591c5caf636074122c835a6c4da04
Tom <tchom.asek@centrum.cz>
2021-03-05 18:59:37 +0100

    final

--- test_calc.py
+++ test_calc.py
@@ -1,24 +1,29 @@
 import unittest
-import calc
+from calc import add, subtract, multiply, divide
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(calc.add(10,5), 15)
-        self.assertEqual(calc.add(-1,1), 0)
-        self.assertEqual(calc.add(-1,-1), -2)
+        self.assertEqual(add(-1,1), 0)
+        self.assertEqual(add(-1,-1), -2)
+        self.assertEqual(add(10,5), 15)
     def test_subtract(self):
-        self.assertEqual(calc.subtract(10,5), 5)
-        self.assertEqual(calc.subtract(-1,1), -2)
-        self.assertEqual(calc.subtract(-1,-1), 0)
+        self.assertEqual(subtract(10,5), 5)
+        self.assertEqual(subtract(-1,1), -2)
+        self.assertEqual(subtract(-1,-1), 0)
     def test_multiply(self):
-        self.assertEqual(calc.multiply(10,5), 50)
-        self.assertEqual(calc.multiply(-1,1), -1)
-        self.assertEqual(calc.multiply(-1,-1), 1)
+        self.assertEqual(multiply(10,5), 50)
+        self.assertEqual(multiply(-1,1), -1)
+        self.assertEqual(multiply(-1,-1), 1)
     def test_divide(self):
-        self.assertEqual(calc.divide(10,5), 2)
-        self.assertEqual(calc.divide(-1,1), -1)
-        self.assertEqual(calc.divide(-1,-1), 1)
+        self.assertEqual(divide(10,5), 2)
+        self.assertEqual(divide(-1,1), -1)
+        self.assertEqual(divide(-1,-1), 1)
+        self.assertEqual(divide(5,2), 2.5)
+        
+        self.assertRaises(ValueError, divide, 10, 0) # option 1
 
+        with self.assertRaises(ValueError): # option 2
+            divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
